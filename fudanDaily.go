0c8ab38f4772c0f65311d30b94915246b2544b2f
zhuzunxiong <zhuzunxiong@bytedance.com>
2022-01-25 18:50:22 +0800

    修正表单数据顺序

--- fudanDaily.go
+++ fudanDaily.go
@@ -17,15 +17,13 @@ import (
 var (
 	client        = &http.Client{}
 	fudanDailyUrl = "https://zlapp.fudan.edu.cn/site/ncov/fudanDaily"
-	loginUrl      = "https://uis.fudan.edu.cn/authserver/login?service=https%3A%2F%2Fzlapp.fudan.edu.cn%2Fa_fudanzlapp%2Fapi" +
-		"%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fzlapp.fudan.edu.cn%252Fsite%252Fncov%252FfudanDaily" +
-		"%26from%3Dwap "
-	getInfoUrl = "https://zlapp.fudan.edu.cn/ncov/wap/fudan/get-info"
-	saveLogUrl = "https://zlapp.fudan.edu.cn/wap/log/save-log"
-	saveUrl    = "https://zlapp.fudan.edu.cn/ncov/wap/fudan/save"
-	userAgent  = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.18(0x17001229) NetType/WIFI Language/zh_CN miniProgram"
-	origin     = "https://zlapp.fudan.edu.cn"
-	Referer    = fudanDailyUrl
+	loginUrl      = "https://uis.fudan.edu.cn/authserver/login?service=https%3A%2F%2Fzlapp.fudan.edu.cn%2Fa_fudanzlapp%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fzlapp.fudan.edu.cn%252Fsite%252Fncov%252FfudanDaily%26from%3Dwap"
+	getInfoUrl    = "https://zlapp.fudan.edu.cn/ncov/wap/fudan/get-info"
+	saveLogUrl    = "https://zlapp.fudan.edu.cn/wap/log/save-log"
+	saveUrl       = "https://zlapp.fudan.edu.cn/ncov/wap/fudan/save"
+	userAgent     = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.18(0x17001229) NetType/WIFI Language/zh_CN miniProgram"
+	origin        = "https://zlapp.fudan.edu.cn"
+	Referer       = fudanDailyUrl
 )
 
 type userInfo struct {
@@ -40,32 +38,28 @@ func setHeader(r *http.Request) {
 	r.Header.Add("Referer", Referer)
 }
 
-func getUrlValue(form map[string]string) url.Values {
-	res := url.Values{}
-	for k, v := range form {
-		res.Add(k, v)
-	}
-	return res
-}
-
 func login(info userInfo) {
 	request, _ := http.NewRequest("GET", loginUrl, nil)
 	setHeader(request)
 	resp, _ := client.Do(request)
-	form := map[string]string{}
+	uv := url.Values{}
 	body, _ := ioutil.ReadAll(resp.Body)
 	h, _ := htmlquery.Parse(strings.NewReader(string(body)))
 	a := htmlquery.Find(h, "//input")
 	for i := range a {
 		name := htmlquery.SelectAttr(a[i], "name")
 		value := htmlquery.SelectAttr(a[i], "value")
-		if name != "" {
-			form[name] = value
+		if name != "" || name == "captchaResponse" {
+			if name == "username" {
+				uv.Add(name, info.Username)
+			} else if name == "password" {
+				uv.Add(name, info.Password)
+			} else {
+				uv.Add(name, value)
+			}
 		}
 	}
-	form["username"] = info.Username
-	form["password"] = info.Password
-	request, _ = http.NewRequest("POST", loginUrl, ioutil.NopCloser(strings.NewReader(getUrlValue(form).Encode())))
+	request, _ = http.NewRequest("POST", loginUrl, ioutil.NopCloser(strings.NewReader(uv.Encode())))
 	setHeader(request)
 	resp, _ = client.Do(request)
 	body, _ = ioutil.ReadAll(resp.Body)
